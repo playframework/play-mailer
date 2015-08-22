99109fb4cec5f6606862338148b5b67f2181264f
chafikhnini <chafik.hnini@gmail.com>
2015-08-22 17:48:32 +0200

    Update Java usage to use fluent interface

--- sample/app/controllers/ApplicationJava.java
+++ sample/app/controllers/ApplicationJava.java
@@ -23,23 +23,23 @@ public class ApplicationJava extends Controller {
   }
 
   public Result send() {
-    final Email email = new Email();
-    email.setSubject("Simple email");
-    email.setFrom("Mister FROM <from@email.com>");
-    email.addTo("Miss TO <to@email.com>");
-    email.addAttachment("favicon.png", new File(Play.application().classloader().getResource("public/images/favicon.png").getPath()));
-    email.addAttachment("data.txt", "data".getBytes(), "text/plain", "Simple data", EmailAttachment.INLINE);
-    email.setBodyText("A text message");
-    email.setBodyHtml("<html><body><p>An <b>html</b> message</p></body></html>");
+    final Email email = new Email()
+      .setSubject("Simple email")
+      .setFrom("Mister FROM <from@email.com>")
+      .addTo("Miss TO <to@email.com>")
+      .addAttachment("favicon.png", new File(Play.application().classloader().getResource("public/images/favicon.png").getPath()))
+      .addAttachment("data.txt", "data".getBytes(), "text/plain", "Simple data", EmailAttachment.INLINE)
+      .setBodyText("A text message")
+      .setBodyHtml("<html><body><p>An <b>html</b> message</p></body></html>");
     String id = mailer.send(email);
     return ok("Email " + id + " sent!");
   }
 
   public Result configureAndSend() {
-    final Email email = new Email();
-    email.setSubject("Simple email");
-    email.setFrom("from@email.com");
-    email.addTo("to@email.com");
+    final Email email = new Email()
+      .setSubject("Simple email")
+      .setFrom("from@email.com")
+      .addTo("to@email.com");
     Map<String, Object> conf = new HashMap<>();
     conf.put("host", "typesafe.org");
     conf.put("port", 1234);
