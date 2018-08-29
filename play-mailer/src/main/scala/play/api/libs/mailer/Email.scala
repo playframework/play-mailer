62733b6855b7c73a4828c41ced83e97cf590a0bc
Logan Nelson <lsnelson33@gmail.com>
2018-08-29 04:25:13 -0500

    Update to Play 2.6.18 (#148)
    * Update Play to version 2.6.18, sbt to 0.13.17, and other shared dependencies
    
    * scalariform updates
    
    * Rolling back mimaPreviousArtifactsVersion to 6.0.0
    
    * Updating sample projects

--- play-mailer/src/main/scala/play/api/libs/mailer/Email.scala
+++ play-mailer/src/main/scala/play/api/libs/mailer/Email.scala
@@ -1,15 +1,15 @@
 package play.api.libs.mailer
 
 case class Email(
-  subject: String,
-  from: String,
-  to: Seq[String] = Seq.empty,
-  bodyText: Option[String] = None,
-  bodyHtml: Option[String] = None,
-  charset: Option[String] = None,
-  cc: Seq[String] = Seq.empty,
-  bcc: Seq[String] = Seq.empty,
-  replyTo: Seq[String] = Seq.empty,
-  bounceAddress: Option[String] = None,
-  attachments: Seq[Attachment] = Seq.empty,
-  headers: Seq[(String, String)] = Seq.empty)
+    subject: String,
+    from: String,
+    to: Seq[String] = Seq.empty,
+    bodyText: Option[String] = None,
+    bodyHtml: Option[String] = None,
+    charset: Option[String] = None,
+    cc: Seq[String] = Seq.empty,
+    bcc: Seq[String] = Seq.empty,
+    replyTo: Seq[String] = Seq.empty,
+    bounceAddress: Option[String] = None,
+    attachments: Seq[Attachment] = Seq.empty,
+    headers: Seq[(String, String)] = Seq.empty)
