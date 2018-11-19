9cbf8459f6b9dc59cca9ad7ef84d0b4ccd8a8251
Marcos Pereira <marcos.silva@gmail.com>
2018-11-19 12:20:07 -0500

    Update, correct and test sample applications

--- samples/runtimeDI/app/controllers/ApplicationJava.java
+++ samples/runtimeDI/app/controllers/ApplicationJava.java
@@ -1,21 +1,23 @@
 package controllers;
 
 import org.apache.commons.mail.EmailAttachment;
-import play.Play;
 import play.api.libs.mailer.MailerClient;
 import play.libs.mailer.Email;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.Environment;
 
 import javax.inject.Inject;
 import java.io.File;
 
 public class ApplicationJava extends Controller {
 
+  private final Environment environment;
   private final MailerClient mailer;
 
   @Inject
-  public ApplicationJava(MailerClient mailer) {
+  public ApplicationJava(Environment environment, MailerClient mailer) {
+    this.environment = environment;
     this.mailer = mailer;
   }
 
@@ -25,7 +27,7 @@ public class ApplicationJava extends Controller {
       .setSubject("Simple email")
       .setFrom("Mister FROM <from@email.com>")
       .addTo("Miss TO <to@email.com>")
-      .addAttachment("favicon.png", new File(Play.application().classloader().getResource("public/images/favicon.png").getPath(), cid))
+      .addAttachment("favicon.png", new File(environment.getFile("public/images/favicon.png"), cid))
       .addAttachment("data.txt", "data".getBytes(), "text/plain", "Simple data", EmailAttachment.INLINE)
       .setBodyText("A text message")
       .setBodyHtml("<html><body><p>An <b>html</b> message with cid <img src=\"cid:" + cid + "\"></p></body></html>");
