2ecd41b6f50053ec900794ea2a526cafdaf16a60
Matthias Kurz <m.kurz@irregular.at>
2021-04-21 10:25:42 +0200

    Upgrading some dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   // scalaVersion needs to be kept in sync with travis-ci
-  val Scala212 = "2.12.11"
-  val Scala213 = "2.13.2"
+  val Scala212 = "2.12.13"
+  val Scala213 = "2.13.5"
   val ScalaVersions = Seq(Scala212, Scala213)
 
   val PlayVersion = sys.props.getOrElse("play.version", sys.env.getOrElse("PLAY_VERSION", "2.8.0"))
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.6")
-addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.6.1")
+addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.8.1")
 
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.3")
 // Even in samples projects
