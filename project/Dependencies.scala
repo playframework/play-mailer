94d9cfa2ba062ac36a2c4f4df1e359b9b45a11ff
Scala Steward <scala_steward@virtuslab.com>
2023-05-29 21:52:54 +0000

    Update scala3-library to 3.3.1-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   // scalaVersion needs to be kept in sync with ci
   val Scala213 = "2.13.10"
-  val Scala3 = "3.3.0-RC6"
+  val Scala3 = "3.3.1-RC1"
   val ScalaVersions = Seq(Scala213, Scala3)
 
   val PlayVersion = sys.props.getOrElse("play.version", sys.env.getOrElse("PLAY_VERSION", "2.9.0-M4"))
