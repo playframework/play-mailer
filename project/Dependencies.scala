d57e8e39951d5996807cbd031213f55ed90833cd
Matthias Kurz <m.kurz@irregular.at>
2023-08-11 13:33:55 +0200

    Scala 3.3.1 / 2.12.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   // scalaVersion needs to be kept in sync with ci
-  val Scala213 = "2.13.11"
-  val Scala3 = "3.3.0"
+  val Scala213 = "2.13.12"
+  val Scala3 = "3.3.1"
   val ScalaVersions = Seq(Scala213, Scala3)
 
   val PlayVersion = sys.props.getOrElse("play.version", sys.env.getOrElse("PLAY_VERSION", "2.9.0-M7"))
