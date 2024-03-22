cc6c835bfc8449316f3950883f5a72da1193c206
Scala Steward <scala_steward@virtuslab.com>
2024-03-22 20:32:37 +0000

    scala3-library 3.3.3 (was 3.3.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   // scalaVersion needs to be kept in sync with ci
   val Scala213 = "2.13.13"
-  val Scala3 = "3.3.1"
+  val Scala3 = "3.3.3"
   val ScalaVersions = Seq(Scala213, Scala3)
 
   val PlayVersion = sys.props.getOrElse("play.version", sys.env.getOrElse("PLAY_VERSION", "3.0.2"))
