d2e076a33b06c65e93c6ad238a270ad35cfaeb87
Matthias Kurz <m.kurz@irregular.at>
2024-10-08 10:03:25 +0200

    Latest Scala versions
    (cherry picked from commit 78b48ff0ec2020219c4c0243e627b5d2befe777a)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   // scalaVersion needs to be kept in sync with ci
-  val Scala213 = "2.13.14"
-  val Scala3 = "3.3.3"
+  val Scala213 = "2.13.15"
+  val Scala3 = "3.3.4"
   val ScalaVersions = Seq(Scala213, Scala3)
 
   val PlayVersion = sys.props.getOrElse("play.version", sys.env.getOrElse("PLAY_VERSION", "2.9.5"))
